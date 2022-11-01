760aab8049550d28f5a3babcde1f3907c0de2bc9
Joan <joan.quispec@gmail.com>
2022-11-01 14:30:10 -0500

    initial develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
-const { Router } = require('express');
-
+const { Router } = require("express");
+const axios = require("axios");
 const router = Router();
 
-
-router.get('/', async (req, res) => {
-	res.send("HOLA MUND")
-})
+router.get("/", async (req, res) => {
+	const info1  = await axios.get(
+		"https://api.rawg.io/api/games?key=8f18e9d52c1a4529b8ffba93f32936dd&page_size=40"
+	  )
+  //console.log(info1.data.results); 
+  res.send(info1.data.results);
+});
 
 module.exports = router;
