2143ea7895549f12436a60694d37c6f884a67749
Matias <matimogica@gmail.com>
2022-11-08 10:03:37 -0300

    pequeños arreglos

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js');
 // const { loadDB } = require('./src/controllers/index')
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     // loadDB()
     console.log('%s listening at 3001'); // eslint-disable-line no-console
